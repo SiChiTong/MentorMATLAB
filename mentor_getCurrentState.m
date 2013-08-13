92c2491880953d5c12cafb40642444872186a2d1
Jared Lewis <jared@jared.kiwi.nz>
2013-08-14 11:02:10 +1200

    Created function to get the state of all 6 mentor axes at once

---
+++ mentor_getCurrentState.m
@@ -0,0 +1,16 @@
+function [ state ] = mentor_getCurrentState(s)
+%MENTOR_GETCURRENTSTATE Queries the mentor of the current position of a all
+%   axes
+
+state = zeros(6,1);
+
+for i = 1:6
+    packet = EncMentorCommand(MentorCMD.READ_POS, axis,3000);
+    fwrite(s, packet);
+    res = fread(s,4);
+
+    state(i) = DecMentorCommand(res);
+end
+
+end
+
