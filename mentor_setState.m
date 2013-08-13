70e4a26e1e1361ef4e2791fe9197ec1f1042a063
Jared Lewis <jared@jared.kiwi.nz>
2013-08-14 11:18:45 +1200

    Added the ability to set all the mentors axes at once

---
+++ mentor_setState.m
@@ -0,0 +1,14 @@
+function [ ] = mentor_setState( mentor, state, enable )
+%MENTOR_SETSTATE Sets the duty cycle for all the axes of the mentor
+%   Calls mentor_setDutyCycle. Enable vector allows individual axes to be
+%   disabled. The enable vector should contain 1's for the enabled axes and
+%   0's for the disabled axes
+
+% Zero the axes that are disabled
+state = state .* enable;
+
+for i=1:6
+    mentor_setDutyCycle(mentor,i,state(i));
+
+end
+
